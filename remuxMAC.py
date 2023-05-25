66746e6a6f7fd46905142f14125b688d0308c3b7
Relepega <rlpggmd@gmail.com>
2023-05-25 21:13:36 +0200

    Imports cleanup and automatic dependency download

--- remuxMAC.py
+++ remuxMAC.py
@@ -1,7 +1,25 @@
 import sys
 import os
 import subprocess
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QPushButton, QFileDialog, QMessageBox
+import shutil
+from PyQt5.QtWidgets import (
+    QApplication,
+    QWidget,
+    QVBoxLayout,
+    QLabel,
+    QLineEdit,
+    QPushButton,
+    QFileDialog,
+    QMessageBox,
+    QDialog
+)
+
+from PyQt5.QtCore import (
+    QThread,
+    QObject,
+)
+
+import urllib.request
 
 video_file = ""
 audio_file = ""
@@ -9,43 +27,86 @@ rimuovi = ""
 script_directory = os.getcwd()
 ffmpeg_executable = os.path.join(script_directory, "ffmpeg")
 
+class Worker(QObject):
+    def __init__(self, window) -> None:
+        super().__init__()
+        self.window = window
 
-def browse_video():
+    def run(self) -> None:
+        # do jobs
+        self.download_ffmpeg()
+
+        # close window
+        self.window.close()
+
+    def download_ffmpeg(self) -> None:
+        # download and unpack logic
+        ffmpeg_zipfile = os.path.join(script_directory, "ffmpeg.zip")
+        urllib.request.urlretrieve("https://evermeet.cx/ffmpeg/getrelease/zip", ffmpeg_zipfile)
+        shutil.unpack_archive(ffmpeg_zipfile)
+
+        # cleanup
+        os.remove(ffmpeg_zipfile)
+
+class DownloadDependencies(QDialog):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.setWindowTitle("Downloading ffmpeg")
+        self.setFixedSize(270, 70)
+
+        # prevent the user from closing the dialog by accident
+        self.setWindowFlag(Qt.WindowCloseButtonHint, False) # type: ignore
+
+        layout = QVBoxLayout()
+
+        label = QLabel("Downloading ffmpeg...\nPlease wait, this may take a while...")
+        label.setAccessibleName("Downloading ffmpeg. Please wait, this may take a while...")  # Set accessible name
+        layout.addWidget(label)
+
+        self.setLayout(layout)
+
+        self.thread = QThread() # type: ignore
+        self.worker = Worker(self)
+        self.worker.moveToThread(self.thread) # type: ignore
+        self.thread.started.connect(self.worker.run) # type: ignore
+        self.thread.start() # type: ignore
+
+def browse_video() -> None:
     global video_file
     video_file, _ = QFileDialog.getOpenFileName(None, "Scegli il Video", "", "Video files (*.avi *.mp4 *.mkv *.mov *.wmv *.flv *.webm *.mpeg *.mpg);;All files (*)")
     video_entry.setText(video_file)
     video_file = f'"{video_file}"'
 
-def browse_audio():
+def browse_audio() -> None:
     global audio_file
     audio_file, _ = QFileDialog.getOpenFileName(None, "Scegli l'audio", "", "Audio files (*.wav *.aiff *.aif *.aifc);;All files (*)")
     audio_entry.setText(audio_file)
     audio_file = f'"{audio_file}"'
 
 
-def run_ffmpeg_remove_audio(video_file):
-    output_file = os.path.splitext(video_file)[0] + "_noaudio.mkv" + '"'
-    cmd = f"{ffmpeg_executable} -i {video_file} -c copy -an {output_file}"
+def run_ffmpeg_remove_audio(video_file) -> None:
+    output_file = os.path.splitext(video_file)[0] + "_noaudio.mkv"
+    cmd = [ffmpeg_executable, "-i", video_file, "-c", "copy", "-an", output_file]
     print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-    print(cmd)
-    subprocess.run(cmd, shell=True)
+    print(' '.join(cmd))
+    subprocess.run(cmd)
 
-def run_ffmpeg_add_audio(video_file, audio_file):
-    output_file = os.path.splitext(video_file)[0] + "_output.mkv" + '"'
-    noau = os.path.splitext(video_file)[0] + "_noaudio.mkv" + '"'
-    cmd = f"{ffmpeg_executable} -i {noau} -i {audio_file} -c copy -map 0 -map 1:a {output_file}"
+def run_ffmpeg_add_audio(video_file, audio_file) -> None:
+    output_file = os.path.splitext(video_file)[0] + "_output.mkv"
+    noau = os.path.splitext(video_file)[0] + "_noaudio.mkv"
+    cmd = [ffmpeg_executable, "-i", noau, "-i", audio_file, "-c", "copy", "-map", "0", "-map", "1:a", output_file]
     print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-    print(cmd)
-    subprocess.run(cmd, shell=True)
+    print(' '.join(cmd))
+    subprocess.run(cmd)
 
-def merge_video_and_audio():
+def merge_video_and_audio() -> None:
     global video_file, audio_file, rimuovi
     run_ffmpeg_remove_audio(video_file)
     run_ffmpeg_add_audio(video_file, audio_file)
-    rimuovi = os.path.splitext(video_file)[0] + "_noaudio.mkv" + '"'
+    rimuovi = os.path.splitext(video_file)[0] + "_noaudio.mkv"
     print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
     print(rimuovi)
-    rimuovi = rimuovi[1:-1]
     os.remove(rimuovi)
     QMessageBox.information(None, "Il mio lavoro qui è finito!", "Video e audio ora SONO UNA COSA SOLA.")
     sys.exit()
